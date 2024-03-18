7c9a32849d038688a7ec95ffd42d140c6595a0e2
that-ugly-cat <98877284+that-ugly-cat@users.noreply.github.com>
2024-03-18 19:24:10 +0100

    Update trp.py

--- trp.py
+++ trp.py
@@ -135,9 +135,9 @@ if st.session_state['login_status']:
             # Group by 'rated_user' and calculate the mean of 'mean_score' for each user
             average_scores_df = ratings_df.groupby('rated_user')['mean_score'].mean()
             # Rename the columns to match the desired output
-            average_scores_df.columns = ['user', 'mean_score']
+            average_scores_df.columns = ['rated_user', 'mean_score']
             # Sort the DataFrame by 'average score' from lowest to highest
-            #average_scores_df = average_scores_df.sort_values(by='mean_score', ascending=True).reset_index()
+            average_scores_df = average_scores_df.sort_values(by='mean_score', ascending=True).reset_index()
             st.write(average_scores_df)
             for user in users:
                 st.write(user)
