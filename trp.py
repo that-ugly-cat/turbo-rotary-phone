c46794b5e623c4a54e8dc1984074aea9e8d7b236
that-ugly-cat <98877284+that-ugly-cat@users.noreply.github.com>
2024-03-19 11:34:33 +0100

    Update trp.py

--- trp.py
+++ trp.py
@@ -120,12 +120,9 @@ if st.session_state['login_status']:
             for key, item in sorted_pool_dict.items(): 
                 st.write(f'-  {item}')
                 if '😉' in item:
-                    explainer_message = 'Se di fianco ad un nome vedi una faccina 😉 significa che hai fatto un\'impressione particolarmente buona.'
                     control_list.append(1)
-                else:
-                    explainer_message = ''
             if len(control_list) > 0:
-                st.write(explainer_message)
+                st.write('Se di fianco ad un nome vedi una faccina 😉 significa che hai fatto un\'impressione particolarmente buona.')
             st.write('\n Ora è il momento di conoscere meglio queste persone, auguri!')
         except: 
             st.write("Non abbiamo ancora calcolato un pool per te, abbi un attimino di pazienza :)")
